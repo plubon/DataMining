8f6f928030ff407de34c3830610e599ecf1daeac
Nibiru-1337 <3psilion@gmail.com>
2017-01-21 16:42:06 +0100

    formatting fix

--- Connection.py
+++ Connection.py
@@ -3,13 +3,13 @@ import csv
 
 class Connection:
     attrs = ['duration','protocol_type', 'service','flag','src_bytes','dst_bytes','land',
-             'wrong_fragment','urgent','hot','num_failed_logins','logged_in','num_compromised','root_shell',
-             'su_attempted','num_root','num_file_creations','num_shells','num_access_files','num_outbound_cmds',
-             'is_host_login','is_guest_login','count','srv_count','serror_rate','srv_serror_rate','rerror_rate',
-             'srv_rerror_rate','same_srv_rate','diff_srv_rate','srv_diff_host_rate','dst_host_count',
-             'dst_host_srv_count','dst_host_same_srv_rate','dst_host_diff_srv_rate','dst_host_same_src_port_rate',
-             'dst_host_srv_diff_host_rate','dst_host_serror_rate','dst_host_srv_serror_rate','dst_host_rerror_rate',
-             'dst_host_srv_rerror_rate', 'label']
+    'wrong_fragment','urgent','hot','num_failed_logins','logged_in','num_compromised','root_shell',
+    'su_attempted','num_root','num_file_creations','num_shells','num_access_files','num_outbound_cmds',
+    'is_host_login','is_guest_login','count','srv_count','serror_rate','srv_serror_rate','rerror_rate',
+    'srv_rerror_rate','same_srv_rate','diff_srv_rate','srv_diff_host_rate','dst_host_count',
+    'dst_host_srv_count','dst_host_same_srv_rate','dst_host_diff_srv_rate','dst_host_same_src_port_rate',
+    'dst_host_srv_diff_host_rate','dst_host_serror_rate','dst_host_srv_serror_rate','dst_host_rerror_rate',
+    'dst_host_srv_rerror_rate', 'label']
     # columns 1, 2, 3, and label enumerations         
     protocol_types = {'tcp':0,'udp':1, 'icmp':2}
     
@@ -24,9 +24,9 @@ class Connection:
     
     flags = {'OTH':0, 'S0':1, 'RSTOS0':2, 'REJ':3, 'SF':4, 'S2':5, 'S3':6, 'SH':7, 'RSTO':8, 'S1':9, 'RSTR':10}
     labels = {'buffer_overflow.':0, 'land.':1, 'rootkit.':2, 'perl.':3, 'multihop.':4, 'spy.':5, 'phf.':6,
-            'guess_passwd.':7,'portsweep.':8, 'pod.':9, 'ipsweep.':10, 'normal.':11, 'teardrop.':12, 'loadmodule.':13,
-            'satan.':14, 'back.':15,'imap.':16, 'smurf.':17,'ftp_write.':18, 'nmap.':19, 'warezclient.':20, 
-            'warezmaster.':21, 'neptune.':22}
+    'guess_passwd.':7,'portsweep.':8, 'pod.':9, 'ipsweep.':10, 'normal.':11, 'teardrop.':12, 'loadmodule.':13,
+    'satan.':14, 'back.':15,'imap.':16, 'smurf.':17,'ftp_write.':18, 'nmap.':19, 'warezclient.':20, 
+    'warezmaster.':21, 'neptune.':22}
 
     def __init__(self, row):
         for idx, val in enumerate(row):
@@ -34,8 +34,6 @@ class Connection:
             if (idx == 1):
                 setattr(self, Connection.attrs[idx], Connection.protocol_types[val])
             elif (idx == 2):
-                print('--------')
-                print(row)
                 setattr(self, Connection.attrs[idx], Connection.services[val])
             elif (idx == 3):
                 setattr(self, Connection.attrs[idx], Connection.flags[val])
