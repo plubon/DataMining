3a8482c2c06cab8331c7d0cf8c1ab7e15f29596f
Nibiru-1337 <3psilion@gmail.com>
2017-01-21 16:37:25 +0100

    patch

--- Connection.py
+++ Connection.py
@@ -10,35 +10,37 @@ class Connection:
              'dst_host_srv_count','dst_host_same_srv_rate','dst_host_diff_srv_rate','dst_host_same_src_port_rate',
              'dst_host_srv_diff_host_rate','dst_host_serror_rate','dst_host_srv_serror_rate','dst_host_rerror_rate',
              'dst_host_srv_rerror_rate', 'label']
-	# columns 1, 2, 3, and label enumerations		 
-	protocol_types = {'tcp':0,'udp':1, 'icmp':2}
-	services = ['http':0, 'domain_u':1, 'gopher':2, 'kshell':3, 'pop_2':4, 'http_8001':5, 'private':6, 'efs':7,
-				'netbios_ssn':8, 'ssh':9, 'smtp':10, 'klogin':11 'vmnet':12, 'daytime':13, 'ftp':14, 'aol':15,
-				'tim_i':16, 'X11':17, 'netstat':18, 'uucp':19, 'pop_3':20, 'Z39_50':21, 'rje':22, 'discard':23,
-				'exec':24, 'uucp_path':25, 'netbios_ns':26, 'ntp_u':27, 'shell':28, 'telnet':29, 'whois':30,
-				'pm_dump':31, 'sunrpc':32, 'csnet_ns':33, 'urh_i':34, 'finger':35, 'other':36, 'eco_i':37,
-				'ldap':38, 'supdup':39, 'domain':40, 'systat':41, 'nnsp':42, 'http_2784':43, 'bgp':44, 'harvest':45,
-				'netbios_dgm':46, 'remote_job':47, 'ctf':48, 'imap4':49, 'urp_i':50, 'name':51, 'time':52,
-				'iso_tsap':53, 'http_443':54, 'printer':55, 'echo':56, 'hostnames':57, 'auth':58, 'sql_net':59,
-				'tftp_u':60, 'ecr_i':61, 'mtp':62, 'link':63, 'red_i':64, 'ftp_data':65, 'nntp':66, 'courier':67,
-				'IRC':68]
-	flags = ['OTH':0, 'S0':1, 'RSTOS0':2, 'REJ':3, 'SF':4, 'S2':5, 'S3':6, 'SH':7, 'RSTO':8, 'S1':9, 'RSTR':10]
-	labels = ['buffer_overflow.':0, 'land.':1, 'rootkit.':2, 'perl.':3, 'multihop.':4, 'spy.':5, 'phf.':6,
-			'guess_passwd.':7,'portsweep.':8, 'pod.':9, 'ipsweep.':10, 'normal.':11, 'teardrop.':12, 'loadmodule.':13,
-			'satan.':14, 'back.':15,'imap.':16, 'smurf.':17,'ftp_write.':18, 'nmap.':19, 'warezclient.':20, 
-			'warezmaster.':21, 'neptune.':22]
+    # columns 1, 2, 3, and label enumerations         
+    protocol_types = {'tcp':0,'udp':1, 'icmp':2}
+    
+    services = {'eco_i':0, 'name':1, 'telnet':2, 'sunrpc':3, 'ftp':4, 'courier':5, 'klogin':6, 'harvest':7,
+    'domain_u':8, 'pop_3':9, 'netstat':10, 'link':11, 'X11':12, 'IRC':13, 'netbios_ssn':14, 'tftp_u':15, 'ldap':16,
+    'kshell':17, 'uucp_path':18, 'http':19, 'aol':20, 'supdup':21, 'shell':22, 'iso_tsap':23, 'red_i':24, 'login':25,
+    'discard':26, 'ftp_data':27, 'rje':28, 'printer':29, 'Z39_50':30, 'sql_net':31, 'daytime':32, 'netbios_ns':33,
+    'http_443':34, 'nntp':35, 'ctf':36, 'efs':37, 'whois':38, 'urh_i':39, 'auth':40, 'ecr_i':41, 'urp_i':42,
+    'http_2784':43, 'nnsp':44, 'ntp_u':45, 'other':46, 'remote_job':47, 'ssh':48, 'time':49, 'tim_i':50, 'echo':51,
+    'pop_2':52, 'domain':53, 'private':54, 'netbios_dgm':55, 'hostnames':56, 'finger':57, 'http_8001':58, 'smtp':59,
+    'uucp':60, 'exec':61, 'vmnet':62, 'mtp':63, 'pm_dump':64, 'bgp':65,'csnet_ns':66, 'systat':67, 'gopher':68, 'imap4':69}
+    
+    flags = {'OTH':0, 'S0':1, 'RSTOS0':2, 'REJ':3, 'SF':4, 'S2':5, 'S3':6, 'SH':7, 'RSTO':8, 'S1':9, 'RSTR':10}
+    labels = {'buffer_overflow.':0, 'land.':1, 'rootkit.':2, 'perl.':3, 'multihop.':4, 'spy.':5, 'phf.':6,
+            'guess_passwd.':7,'portsweep.':8, 'pod.':9, 'ipsweep.':10, 'normal.':11, 'teardrop.':12, 'loadmodule.':13,
+            'satan.':14, 'back.':15,'imap.':16, 'smurf.':17,'ftp_write.':18, 'nmap.':19, 'warezclient.':20, 
+            'warezmaster.':21, 'neptune.':22}
 
     def __init__(self, row):
         for idx, val in enumerate(row):
             # 1 -> 3 str columns, last is str label
-			if (idx == 1):
-				setattr(self, Connection.attrs[idx], protocol_types[val])
-			elif (idx == 2):
-				setattr(self, Connection.attrs[idx], services[val])
-			elif (idx == 3):
-				setattr(self, Connection.attrs[idx], flags[val])
-			elif (idx == 41):
-				setattr(self, Connection.attrs[idx], labels[val])
+            if (idx == 1):
+                setattr(self, Connection.attrs[idx], Connection.protocol_types[val])
+            elif (idx == 2):
+                print('--------')
+                print(row)
+                setattr(self, Connection.attrs[idx], Connection.services[val])
+            elif (idx == 3):
+                setattr(self, Connection.attrs[idx], Connection.flags[val])
+            elif (idx == 41):
+                setattr(self, Connection.attrs[idx], Connection.labels[val])
             # 24 -> 30 float columns
             elif (24 <= idx and idx <= 30):
                 setattr(self, Connection.attrs[idx], float(val))
